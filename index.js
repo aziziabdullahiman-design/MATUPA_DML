6375a10382492425fe2e93cd57fe8ecbd37a4140
aziziabdullahiman-design <aziziabdullahiman@gmail.com>
2026-08-26 00:44:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-{
-  "name": "matupa-dml-bot",
-  "version": "1.0.0",
-  "description": "WhatsApp bot",
-  "main": "index.js",
-  "scripts": {
-    "start": "node index.js"
-  },
-  "dependencies": {
-    "@whiskeysockets/baileys": "^6.7.9",
-    "pino": "^9.3.2"
-  }
-}
+console.log("Matupa DML Bot imeanza!");
